a292c71d285e4dc4f1afedd21c4522abfd7836f9
rain <31737634+mdepp@users.noreply.github.com>
2023-01-01 21:06:21 +0000

    Add teleoperation

--- src/toplevel/launch/robot.launch.py
+++ src/toplevel/launch/robot.launch.py
@@ -1,11 +1,32 @@
-from launch_ros.actions import Node
+import os
 
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
 
 def generate_launch_description():
+    TURTLEBOT3_MODEL = os.environ["TURTLEBOT3_MODEL"]
+
+    tb3_param_dir = LaunchConfiguration(
+        "tb3_param_dir",
+        default=os.path.join(
+            get_package_share_directory("turtlebot3_bringup"),
+            "param",
+            TURTLEBOT3_MODEL + ".yaml",
+        ),
+    )
+
     return LaunchDescription(
         [
+            Node(
+                package="turtlebot3_node",
+                executable="turtlebot3_ros",
+                parameters=[tb3_param_dir],
+                arguments=["-i", "/dev/ttyACM0"],
+                output="screen",
+            ),
             Node(
                 package="cv_camera",
                 executable="cv_camera_node",
